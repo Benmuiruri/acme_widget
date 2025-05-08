718b44a7323b25c146c3d1dd43256b3a306696fc
Ben Kiarie <kiarie@medic.org>
2025-05-08 11:58:20 +0300

    chore: add basket spec

---
+++ spec/acme_widget/basket_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+require_relative '../../lib/acme_widget/models/product'
+require_relative '../../lib/acme_widget/models/catalog'
+require_relative '../../lib/acme_widget/basket'
+
+RSpec.describe AcmeWidget::Basket do
+  let(:red_widget) { AcmeWidget::Product.new('R01', 32.95) }
+  let(:green_widget) { AcmeWidget::Product.new('G01', 24.95) }
+  let(:blue_widget) { AcmeWidget::Product.new('B01', 7.95) }
+  let(:catalog) { AcmeWidget::Catalog.new([red_widget, green_widget, blue_widget]) }
+  let(:basket) { AcmeWidget::Basket.new(catalog: catalog) }
+
+  describe '#add' do
+    it 'adds a product to the basket' do
+      basket.add('R01')
+      expect(basket.items).to include(red_widget)
+    end
+
+    it 'raises an error for invalid product code' do
+      expect { basket.add('X99') }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe '#subtotal' do
+    it 'returns the sum of all item prices' do
+      basket.add('R01')
+      basket.add('G01')
+      expect(basket.subtotal).to eq(red_widget.price + green_widget.price)
+    end
+  end
+
+  describe '#total' do
+    it 'returns the subtotal initially' do
+      basket.add('B01')
+      basket.add('G01')
+      expect(basket.total).to eq(blue_widget.price + green_widget.price)
+    end
+  end
+
+  describe '#clear' do
+    it 'removes all items from the basket' do
+      basket.add('R01')
+      basket.clear
+      expect(basket.items).to be_empty
+    end
+  end
+end
